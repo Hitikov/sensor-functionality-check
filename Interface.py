878210750d3484c1d9aecffeffaf8757be28f6c4
MMblW <147233972+MMblW@users.noreply.github.com>
2025-06-19 13:55:34 +0500

    Добавлены функции для считывания .csv и для вывода данных в виде .xlsx

---
+++ Interface.py
@@ -0,0 +1,36 @@
+import csv
+import xlsxwriter
+
+def ReadFile(path):
+    with open(path, newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter=',')
+        return ReaderToList(filereader)
+        
+            
+def ReaderToList(filereader):
+    datalist = []
+    for row in filereader:
+        datalist.append(tuple(row))
+    return datalist
+
+def XlsxOutput(datalist):
+    print("Enter filename")
+    name = str(input())
+    workbook = xlsxwriter.Workbook(name + '.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    col = 0
+    for item1, item2 in (datalist):
+        worksheet.write(row, col, item1)
+        worksheet.write(row, col + 1, item2)
+        row += 1
+    workbook.close()
+
+
+
+# Example call
+print("Enter path")    
+path = str(input())
+datalist = ReadFile(path)
+XlsxOutput(datalist)
