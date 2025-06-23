fb00d514f7c5552489ff2e832c1ee157fe6a4184
MMblW <147233972+MMblW@users.noreply.github.com>
2025-06-23 14:58:04 +0500

    Подписаны оси координат графика. Создан шаблон для записи результатов анализа в xlsx

--- Graphics.py
+++ Graphics.py
@@ -1,31 +1,50 @@
-from PIL import Image, ImageDraw, ImageFont
+﻿from PIL import Image, ImageDraw, ImageFont
 from Interface import *
-from Data_processing import SliceData
+from Data_processing import *
 
 def CreateBlankGraphic():
     graphic = Image.new('1', [900,1100], 'white')
     draw = ImageDraw.Draw(graphic)
     
     #Drawing main borders
-    draw.line([(100, 1000), (100, 0)], fill='black', width=2)
-    draw.line([(100, 1000), (900, 1000)], fill='black', width=2)
+    draw.line([(100, 1000), (100, 0)], fill='black', width=4)
+    draw.line([(100, 1000), (900, 1000)], fill='black', width=4)
     
-    #Font and text size
+    #Set font and text size
+    currentFont = ImageFont.truetype('arial.ttf', size=32)
+
+    #Sign axis
+    currentText = "Длина волны, нм"
+    draw.text((350, 1050), currentText, font = currentFont, fill = 'black')
+    
+    currentText = "Оптический сигнал, дБм"
+    fontimage = Image.new('1', [380, 40], 'black')
+    ImageDraw.Draw(fontimage).text((0, 0), currentText, font = currentFont, fill = 'white')
+    fontimage = fontimage.rotate(90, expand=True)
+    graphic.paste('black', box=(20, 350), mask=fontimage)
+
+    #Change font and text size
     currentFont = ImageFont.truetype('arial.ttf', size=20)
     
     for i in range(5):
         #Drawing grid
-        draw.line([(100, 1200 - ((i + 1) * 200)), (900, 1200 - ((i + 1) * 200))], fill='black', width=1)
+        draw.line([(100, 1200 - ((i + 1) * 200)), (900, 1200 - ((i + 1) * 200))], fill='black', width=2)
         #Drawing text
         currentText = str(-60 + (i * 10))
         draw.text((60, 990 - (200 * i)), currentText, font = currentFont, fill = 'black')
         
     for i in range(4):
         #Drawing grid
-        draw.line([(100 + (i + 1) * 200, 1000), (100 + (i + 1) * 200, 0)], fill='black', width=1)
+        draw.line([(100 + (i + 1) * 200, 1000), (100 + (i + 1) * 200, 0)], fill='black', width=2)
         #Drawing text
         currentText = str(1500 + (i * 25))
         draw.text(((80 + (i * 200)), 1010), currentText, font = currentFont, fill = 'black')
+        
+    #Drawing secondary grid
+    for i in range(25):
+        draw.line([(100, 1000 - ((i + 1) * 40)), (900, 1000 - ((i + 1) * 40))], fill='black', width=1)
+    for i in range(25):
+        draw.line([(100 + (i + 1) * 40, 1000), (100 + (i + 1) * 40, 0)], fill='black', width=1)
     
     return graphic
 
@@ -52,3 +71,4 @@ datalist = SliceData(datalist, 1500, 1600)
 img = CreateGraphic(datalist) 
 XlsxOutput(datalist, "1")
 img.save("2.png")
+AnalyseData(datalist, "C:\\Users\\Admin\\Desktop\\Tests\\Test.xlsx")
--- Interface.py
+++ Interface.py
@@ -1,5 +1,9 @@
+﻿import os
 import csv
 import xlsxwriter
+from openpyxl import load_workbook, cell
+from Data_processing import *
+from Function_characteristics import *
 
 def ReadFile(path):
     with open(path, newline='') as csvfile:
@@ -10,6 +14,8 @@ def ReaderToList(filereader):
     datalist = []
     for row in filereader:
         datalist.append(tuple(row))
+    datalist = SliceData(datalist, 1500, 1600)
+    datalist = convert_to_float(datalist)
     return datalist
 
 def XlsxOutput(datalist, filename):
@@ -20,7 +26,51 @@ def XlsxOutput(datalist, filename):
     row = 0
     col = 0
     for item1, item2 in (datalist):
-        worksheet.write(row, col, item1)
-        worksheet.write(row, col + 1, item2)
+        worksheet.write(row, col, float(item1))
+        worksheet.write(row, col + 1, float(item2))
         row += 1
     workbook.close()
+
+def AnalyseData(datalist, path): 
+    #Intial create
+    if not(os.path.exists(path)):   
+        workbook = xlsxwriter.Workbook(path)
+        worksheet = workbook.add_worksheet()
+
+        #Field labels
+        worksheet.write(0, 0, "Номер с примечанием")
+        worksheet.write(0, 1, "Потери, дБ")
+        worksheet.write(0, 2, "Контраст интерференции, дБ")
+        worksheet.write(0, 3, "FSR")
+        worksheet.write(0, 4, "Спектр")
+        worksheet.write(0, 5, "Состояние")
+
+        workbook.close()
+
+    #Find first free row
+    workbookLoader = load_workbook(filename = path)
+    sheet = workbookLoader.active
+    freeRow = sheet.max_row + 1
+
+    number = "0"
+    losses, contrast, FSR = dataset_analysis(datalist)
+    spectre_link = "0"
+    condition = "0"
+
+    data = [number, losses, contrast, FSR, spectre_link, condition]
+    for i in range(5):
+        cell = sheet.cell(freeRow, i + 1).value = data[i]
+
+    workbookLoader.save(path)
+    return 0      
+
+
+#Example call
+print("Enter path")    
+path = str(input())
+datalist = ReadFile(path)
+datalist = SliceData(datalist, 1500, 1600)
+#img = CreateGraphic(datalist) 
+XlsxOutput(datalist, "1")
+#img.save("2.png")
+AnalyseData(datalist, "C:\\Users\\Admin\\Desktop\\Tests\\Test.xlsx")
