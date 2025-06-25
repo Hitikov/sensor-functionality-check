c92aa355ea560fd4c07a87f367f05884b3bfcfd0
MMblW <147233972+MMblW@users.noreply.github.com>
2025-06-25 12:18:48 +0500

    Remade saving of single files, fixed graphic bugs

--- Interface.py
+++ Interface.py
@@ -2,7 +2,7 @@
 import os
 
 import xlsxwriter
-from openpyxl import load_workbook
+from openpyxl import load_workbook, styles
 
 from Function_characteristics import *
 from Graphics import *
@@ -46,7 +46,6 @@ def AnalyseData(datalist, xlsxpath, imgpath, record_name):
         worksheet.write(0, 2, "Контраст интерференции, дБ")
         worksheet.write(0, 3, "FSR")
         worksheet.write(0, 4, "Спектр")
-        worksheet.write(0, 5, "Состояние")
 
         workbook.close()
     
@@ -63,12 +62,15 @@ def AnalyseData(datalist, xlsxpath, imgpath, record_name):
     losses, contrast, FSR, tempvariable = dataset_analysis(datalist)
     spectre_link = imgpath
     if dataset_validation([losses, contrast], -20, 7):
-        condition = "Годен"
+        fillColor = styles.colors.Color(rgb='0000FF00')
     else:
-        condition = "Негоден"
-    data = [record_name, losses, contrast, FSR, spectre_link, condition]
-    for i in range(6):
+        fillColor = styles.colors.Color(rgb='00FF0000')
+    data = [record_name, losses, contrast, FSR, spectre_link]
+    for i in range(5):
         cell = sheet.cell(freeRow, i + 1).value = data[i]
+    fill = styles.fills.PatternFill(patternType='solid', fgColor=fillColor)
+    for i in range(5):
+        cell = sheet.cell(freeRow, i + 1).fill = fill
 
     workbookLoader.save(xlsxpath)
 
